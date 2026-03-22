c19318f8b293ee4df983a685b970f466bff6dfab
Mansi Kapri <85777718+Mansikapri@users.noreply.github.com>
2026-03-22 15:01:37 +0530

    Create word.py
    Adding the words used for suggestions in the game.

---
+++ Hangman/word.py
@@ -0,0 +1,15 @@
+fruits= "apple mango banana jackfruit lemon orange pineapple melon papaya cherry peach litchi grapes berries watermelon cheeku lime blueberry strawberry coconut"
+"pear apricot avocado blackberry grapefruit kiwi plum raspberry pomegranete fig tangerine"
+fruit_list=fruits.split(" ")
+
+vegetables="carrot broccoli asparagus cauliflower corn cucumber lettus mushrooms pumpkin"
+"beetroot peas zucchini radish cabbage chili garlic basil coriander parsley oregano eggplant potato tomato onion"
+"cinnamon saffron bean chickpea lentil"
+
+veg_list=vegetables.split(" ")
+
+cars="baleno ertia swift alto celerio ciaz ignis venue creta santro verna xcent elantra xuv500"
+"nano tiago harrier zest fortuner duster capture kwid seltos rio amaze jazz mustang figo hector polo"
+"montero micra maraazo"
+
+car_list=cars.split(" ")
